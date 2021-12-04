9c6ba8698e26405d17fa4d3185696009305f3871
dantolvil <danieltoledovillalba@gmail.com>
2021-12-04 22:50:36 +0100

    Cambios repositorios

--- src/main/java/org/springframework/samples/peliculasOnline/repository/AuthoritiesRepository.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.samples.peliculasOnline.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.peliculasOnline.model.Authorities;
-
-public interface AuthoritiesRepository extends  CrudRepository<Authorities, String>{
-	
-}
